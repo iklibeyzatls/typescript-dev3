e3857bbef23e459b28e291724d54e243c5711a42
iklibeyzatls <iklilbeyzatalas@gmail.com>
2025-11-07 19:40:28 +0300

    Add sum function to calculate total of numbers

---
+++ soru8.ts
@@ -0,0 +1,8 @@
+// Soru8.ts
+
+function sum(...numbers: number[]): number {
+  return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log(sum(1, 2, 3));        // 6
+console.log(sum(10, 20, 30, 40)); // 100
